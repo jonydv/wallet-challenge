afc77cd2c2cee78fd3dd4943833385a734c1c1c8
Jonatan Villalba <jonatandavidvillalba@gmail.com>
2022-05-07 08:50:43 -0300

    add new dependencies to the project and create the folder wallets with his controllers, service and module

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { WalletsModule } from './wallets/wallets.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [WalletsModule],
 })
 export class AppModule {}
