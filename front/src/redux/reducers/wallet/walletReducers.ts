3aca80f75aa599dc148b5067d22843c4db297606
Jonatan Villalba <jonatandavidvillalba@gmail.com>
2022-05-08 23:40:41 -0300

    add types and logic to the walletReducers

--- front/src/redux/reducers/wallet/walletReducers.ts
+++ front/src/redux/reducers/wallet/walletReducers.ts
@@ -1,10 +1,11 @@
+import { WalletI } from '../../../models/wallet.interface';
 import { WalletActionType } from '../../action-types/wallet/wallet-action-types';
 import { Action } from '../../actions';
 
 interface WalletState {
     loading: boolean;
     error: string | null;
-    walletInfo?: any;
+    walletInfo?: WalletI[] | null;
 };
 
 const initialState: WalletState = {
@@ -13,52 +14,88 @@ const initialState: WalletState = {
     walletInfo: []
 };
 
-export const getWalletsReducer = (state: WalletState = initialState, action: Action): WalletState => {
+export const walletsReducer = (state: WalletState = initialState, action: Action): WalletState => {
     switch (action.type) {
         case WalletActionType.WALLET_GET_ALL_REQUEST:
             return {
+                ...state,
                 loading: true,
-                walletInfo: null,
-                error: null
             };
         case WalletActionType.WALLET_GET_ALL_SUCCESS:
             return {
+                ...state,
                 loading: false,
                 walletInfo: action.payload,
-                error: null
             };
         case WalletActionType.WALLET_GET_ALL_FAIL:
             return {
+                ...state,
                 loading: false,
-                walletInfo: null,
                 error: action.payload
             };
+
+        case WalletActionType.WALLET_CREATE_REQUEST:
+            return {
+                ...state,
+                loading: true,
+            };
+         case WalletActionType.WALLET_CREATE_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+                walletInfo: [...state?.walletInfo!, action?.payload],
+            };
+        
+        case WalletActionType.WALLET_CREATE_FAIL:
+            return {
+                ...state,
+                loading: false,
+                error: action.payload
+            };
+
+            
+        case WalletActionType.WALLET_UPDATE_ISFAVORITE_REQUEST:
+            return {
+                ...state,
+                loading: true,
+                };
+        case WalletActionType.WALLET_UPDATE_ISFAVORITE_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+                walletInfo: state?.walletInfo?.map!(
+                    wallet =>  (wallet._id === action.payload._id)  ? action.payload : wallet
+                ),              
+                };
+        case WalletActionType.WALLET_UPDATE_ISFAVORITE_FAIL:
+                return {
+                    ...state,
+                    loading: false,
+                    error: action.payload
+                };
+
+        case WalletActionType.WALLET_REMOVE_REQUEST:
+            return {
+                ...state,
+                loading: true,
+                };
+        case WalletActionType.WALLET_REMOVE_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+                walletInfo: state?.walletInfo?.filter!(
+                    wallet =>  wallet._id !== action.payload._id
+                ),              
+                };
+        case WalletActionType.WALLET_REMOVE_FAIL:
+                return {
+                    ...state,
+                    loading: false,
+                    error: action.payload
+                };
+
         default:
             return state;
     }
 };
 
-export const createWalletReducer = (state: WalletState = initialState, action: Action): WalletState => {
-    switch (action.type) {
-        case WalletActionType.WALLET_CREATE_REQUEST:
-            return {
-                loading: true,
-                walletInfo: null,
-                error: null
-            };
-        case WalletActionType.WALLET_CREATE_SUCCESS:
-            return {
-                loading: false,
-                walletInfo: action.payload,
-                error: null
-            };
-        case WalletActionType.WALLET_CREATE_FAIL:
-            return {
-                loading: false,
-                walletInfo: null,
-                error: action.payload
-            };
-        default:
-            return state;
-    }
-};
